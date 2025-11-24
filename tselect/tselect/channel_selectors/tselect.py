363c9ad5d924f5703ff2eec8b38eb55310eb93fa
Loren Nuyts <loren@ysgarden.com>
2025-11-24 10:40:19 +0100

    debug info

--- tselect/tselect/channel_selectors/tselect.py
+++ tselect/tselect/channel_selectors/tselect.py
@@ -857,7 +857,13 @@ class TSelect(TransformerMixin):
         if i == 0:
             return data_to_filter
         threshold = self.evaluation_metric_per_channel[self.sorted_scores[-i]]  # the threshold below which we will delete items
-        first_ix = self.sorted_scores.index(threshold)
+        try:
+            first_ix = self.sorted_scores.index(threshold)
+        except ValueError as e:
+            print(f"Sorted scores: {self.sorted_scores}")
+            print(f"Indexed threshold: {threshold}")
+            print(f"Index in list: {-i}")
+            raise e
         true_percentage = first_ix / len(self.evaluation_metric_per_channel.keys())
         print(f" True percentage: {true_percentage}, desired percentage: {p}")
         if true_percentage < p + 0.01:
